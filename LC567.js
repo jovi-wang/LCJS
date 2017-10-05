bcb68857287709ce0e91f2ea058079a5a6a456cd
Jovi Wang <jovi.wang@readify.net>
2017-10-05 18:39:43 +1100

    1

--- LC567.js
+++ LC567.js
@@ -5,19 +5,32 @@
  */
 var checkInclusion = function(s1, s2) {
   if(s1.length>s2.length) return false;
-  const count=new Map();
+  const s1map=new Array(26);
+  s1map.fill(0);
   for(let i=0;i<s1.length;i++){
-      let char = s1[i];
-      if(count.get(char)){
-          count.set(char,count.get(char)+1);
-      }
-      else count.set(char,1);
+      let index=s1.charCodeAt(i)-'a'.charCodeAt(0);
+      s1map[index]++;
   }
-  let cnt=s1.length;
-  let left=0, right=0;
+  for(let i=0;i<=s2.length-s1.length;i++){
+      const s2map=new Array(26);
+      s2map.fill(0);
+      for(let j=0;j<s1.length;j++){
+          let index=s2.charCodeAt(i+j)-'a'.charCodeAt(0);
+          s2map[index]++;
+      }
+      console.log(s2map)
+      if(match(s1map,s2map)) return true;
+  }
   
-  console.log(count);
+  return false;
 
 };
 
-console.log(checkInclusion('asssssd','sdsdfasdfasdf'))
+const match=(s1map,s2map)=>{
+    for(let i=0;i<s1map.length;i++){
+        if (s1map[i] !== s2map[i]) return false;
+    }
+    return true;
+};
+
+console.log(checkInclusion('ac','sdica'))
