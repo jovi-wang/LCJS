0a82b1fe96270df670fdb1606203b257350f0092
jovi <jovi_wang@outlook.com>
2018-01-23 21:57:42 +1100

    update 1

--- LC1.js
+++ LC1.js
@@ -19,7 +19,7 @@ var twoSum = function(nums, target) {
 /* Runtime: 102 ms*/
     for(let i=0;i<nums.length;i++){
         console.log(12)
-        let temp = target-nums[i];
+        let temp = target-nums[i]+1;
             let j = nums.lastIndexOf(temp);
             if(j>-1 && j!==i){
                 return [i,j];
