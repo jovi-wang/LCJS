de507d960afc3b40d8bf9091ad5929e43071d6c5
jovi-wang <jovi_wang@outlook.com>
2018-01-23 21:45:22 +1100

    Update test.js

--- test.js
+++ test.js
@@ -68,21 +68,6 @@ var number = function(busStops){
     // busStops.reduce((rem, [on, off]) => rem + on - off, 0);
   
 };
-// console.log(number([[10,0],[3,5],[5,8]]));
-
-function order(words){
-  // ...
-    /*
-    const array=words.split(' ');
-    array.sort((a,b)=>{
-      return parseInt(a.replace(/[^1-9]/ig,'')) - parseInt(b.replace(/[^1-9]/ig,''));
-    });
-    return array.join(' ');
-    */
-    return words.split(' ').sort(function(a, b){
-      return a.match(/\d/) - b.match(/\d/);
-   }).join(' ');
-}
 //order("is2 Thi1s T4est 3a")=== "Thi1s is2 3a T4est"
 function findLongest(array){
   // code here
